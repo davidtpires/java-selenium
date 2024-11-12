17d8b64c9e4b3edfd663e8cee7ed6b25c0ef11f6
David T. <davidt.info@gmail.com>
2024-11-12 15:10:42 -0300

    Testando o cadastro de leilão

--- src/test/java/br/com/alura/leilao/leiloes/LeiloesTest.java
+++ src/test/java/br/com/alura/leilao/leiloes/LeiloesTest.java
@@ -1,9 +1,13 @@
 package br.com.alura.leilao.leiloes;
 
 import br.com.alura.leilao.login.LoginPage;
+import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class LeiloesTest {
 
@@ -20,6 +24,13 @@ public class LeiloesTest {
         paginaDeLogin.preencheFormularioDeLogin("fulano", "pass");
         this.paginaDeLeiloes = paginaDeLogin.efetuaLogin();
         CadastroLeilaoPage paginaDeCadastro = paginaDeLeiloes.carregarFormulario();
+
+        String hoje = LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        String nome = "Leilao do dia " + hoje;
+        String valor = "500.00";
+
+        this.paginaDeLeiloes = paginaDeCadastro.cadastrarLeilao(nome, valor, hoje);
+        Assert.assertTrue(paginaDeLeiloes.isLeilaoCadastrado(nome, valor, hoje));
 
     }
 }
